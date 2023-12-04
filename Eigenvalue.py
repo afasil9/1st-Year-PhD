232c96a20b273f3fe14e9e596f39e63d6dde4be7
IgorBaratta <igorbaratta@gmail.com>
2023-12-04 14:14:07 +0000

    update code

--- Eigenvalue.py
+++ Eigenvalue.py
@@ -2,14 +2,7 @@
 from ufl import dx, curl, inner, TrialFunction, TestFunction
 import numpy as np
 from dolfinx import fem, mesh
-from dolfinx.fem import (dirichletbc, Function, FunctionSpace, form,
-                         VectorFunctionSpace, locate_dofs_topological)
 from mpi4py import MPI
-from dolfinx.fem.petsc import assemble_matrix
-from petsc4py import PETSc
-from dolfinx.mesh import (create_rectangle, locate_entities_boundary,
-                          CellType, GhostMode, DiagonalType)
-import sys
 from scipy import linalg
 from scipy.sparse.linalg import eigs
 
@@ -21,46 +14,28 @@ fdim = domain.topology.dim - 1
 
 facets=mesh.locate_entities_boundary(domain,fdim,lambda x:np.full(x.shape[1],True,dtype=bool))
 
-V = fem.FunctionSpace(domain, ("Lagrange", 1, (gdim,)))
+V = fem.functionspace(domain, ("Lagrange", 1, (gdim,)))
 # V = fem.FunctionSpace(domain, ("N1curl", 1))
 
 dofs = fem.locate_dofs_topological(V=V, entity_dim=fdim, entities=facets)
 
-# cond = fem.Function(V)
-# cond.interpolate(lambda x: np.vstack((np.sin(np.pi*x[0]), np.sin(np.pi*x[1]))))
-# bcs = fem.dirichletbc(cond, dofs)
+bcs = fem.dirichletbc(np.array([0.0, 0.0], dtype=np.float64), dofs, V)
 
 # Define problem
 u = TrialFunction(V)
 v = TestFunction(V)
-a = form(inner(curl(u), curl(v)) * dx)
-b = form(inner(u, v) * dx)
+a = fem.form(inner(curl(u), curl(v)) * dx)
+b = fem.form(inner(u, v) * dx)
 
 # Assemble matrices
-A = assemble_matrix(a,[bcs])
-A.assemble()
+A = fem.assemble_matrix(a,[bcs])
 # Zero rows of boundary DOFs of B. See [1]
-B = assemble_matrix(b, [bcs], diagonal =0.0)
-B.assemble()
+B = fem.assemble_matrix(b, [bcs])
 
-# View Matrix
-viewer_a = PETSc.Viewer().createASCII('assembled_matrix_A.txt', 'w')
-A.view(viewer_a)
-
-viewer_b = PETSc.Viewer().createASCII('assembled_matrix_B.txt', 'w')
-B.view(viewer_b)
-
-# # 
-def petsc2array(v):
-    s=v.getValues(range(0, v.getSize()[0]), range(0,  v.getSize()[1]))
-    return s
+Asp = A.to_scipy()
+Bsp = B.to_scipy()
 
 
-np_a = petsc2array(A)
-np_b = petsc2array(B)
+w, v = eigs(Asp, k=6, M=Bsp)
 
-# np.fill_diagonal(M, np.where(np.diagonal(M) == 0, 1e-12, np.diagonal(M)))
-
-# test = eigs(A = np_a, k =12, M= np_b)
-
-# %%
+print(w)
