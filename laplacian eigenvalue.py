4ead6478c9b8464c951aa37c2488f70d73e46237
Arshad Fasiludeen <137993931+afasil9@users.noreply.github.com>
2023-12-15 09:45:28 +0000

    Update laplacian eigenvalue.py
    Changes made with boundary conditions implementation on the matrix.

--- laplacian eigenvalue.py
+++ laplacian eigenvalue.py
@@ -1,127 +1,76 @@
 #%%
-from ufl import dx, curl, inner, TrialFunction, TestFunction, SpatialCoordinate, grad
-import ufl
+import dolfinx, ufl
 import numpy as np
-from dolfinx import fem, mesh
-from dolfinx.fem import (dirichletbc, Function, FunctionSpace, form,
-                         VectorFunctionSpace, locate_dofs_topological)
+import scipy.sparse as sps
+import scipy.sparse.linalg as la
 from mpi4py import MPI
-from dolfinx.fem.petsc import assemble_matrix, assemble_vector
-from petsc4py import PETSc
-from dolfinx.mesh import (create_unit_square, locate_entities_boundary,
-                          CellType, GhostMode, DiagonalType)
-from petsc4py.PETSc import ScalarType
-import sys
-from scipy import linalg
-from scipy.sparse.linalg import eigs
-from dolfinx.fem.petsc import LinearProblem
+from dolfinx import mesh
+from ufl import form
+from dolfinx.fem import form
 
-n = 2
-domain = mesh.create_unit_square(MPI.COMM_WORLD, n, n)
-x = SpatialCoordinate(domain)
-gdim = domain.geometry.dim
-fdim = domain.topology.dim - 1
+n=2
+domain = mesh.create_unit_square(MPI.COMM_WORLD, n, n, mesh.CellType.quadrilateral)
 
-facets = mesh.locate_entities_boundary(domain, fdim, marker= lambda x:np.logical_or(np.isclose(x[0], 0.0),
-        np.isclose(x[0], 1.0)))
+FE=ufl.FiniteElement("Lagrange", domain.ufl_cell(),1)
 
-V = fem.FunctionSpace(domain, ("Lagrange",1))
-dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
+V=dolfinx.fem.FunctionSpace(domain, ("Lagrange", 1))
+u=ufl.TrialFunction(V)
+v=ufl.TestFunction(V)
 
-bc = fem.dirichletbc(value=ScalarType(0), dofs=dofs, V=V)
-
-#Funciton spaces
-
-u = TrialFunction(V)
-v = TestFunction(V)
-a = form(inner(grad(u), grad(v)) * dx)  
-
-
-sp = fem.create_sparsity_pattern(a)
-
-# f = 6 #10 * ufl.exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
-L = form(inner(u, v) * dx)
-
-A = assemble_matrix(a, bcs = [bc])
-A.assemble()
-
-B = assemble_matrix(L)
-B.assemble()
+# Defining the forms
+a=form(ufl.inner(ufl.grad(u),ufl.grad(v))*ufl.dx)
+b=form(ufl.inner(u,v)*ufl.dx)
 
 def petsc2array(v):
     s=v.getValues(range(0, v.getSize()[0]), range(0,  v.getSize()[1]))
     return s
+#%%
+# Boundary Condtions
 
-np_a = petsc2array(A)
-np_b = petsc2array(B)
-
-#np_b = B.getArray() #Use this if B was a vector
-
-# Identiy 
-
-len = np_a.shape[0]
-I = np.identity(len)
-
-from scipy.linalg import eigvals
-
-eigvals(np_a)
-
-# %%
-# Eigenvalue stuff Attempt 1
-
-from slepc4py import SLEPc
-
-n_eigs = 12
-
-eps = SLEPc.EPS().create(PETSc.COMM_WORLD)
-eps.setOperators(A, B)
-eps.setType(SLEPc.EPS.Type.ARNOLDI)
-eps.setProblemType(SLEPc.EPS.ProblemType.GHEP)
-eps.setWhichEigenpairs(eps.Which.TARGET_MAGNITUDE)
-
-st = eps.getST()
-st.setType(SLEPc.ST.Type.SINVERT)
-
-eps.setDimensions(n_eigs, PETSc.DECIDE, PETSc.DECIDE)
-eps.setFromOptions()
-eps.solve()
-
-its = eps.getIterationNumber()
-eps_type = eps.getType()
-n_ev, n_cv, mpd = eps.getDimensions()
-tol, max_it = eps.getTolerances()
-n_conv = eps.getConverged()
-
-computed_eigenvalues = []
-for i in range(min(n_conv, n_eigs)):
-    lmbda = eps.getEigenvalue(i)
-    computed_eigenvalues.append(np.round(np.real(lmbda), 1))
-
-print(f"Number of iterations: {its}")
-print(f"Solution method: {eps_type}")
-print(f"Number of requested eigenvalues: {n_ev}")
-print(f"Stopping condition: tol={tol}, maxit={max_it}")
-print(f"Number of converged eigenpairs: {n_conv}")
-
-print(np.sort(computed_eigenvalues))
-
-# %%
-#Attempt 2
-
-n = 10 # No of Eigenvalues
-
-eigensolver = SLEPc.EPS().create(MPI.COMM_WORLD)
-eigensolver.setOperators(A,B)
-eigensolver.setType(SLEPc.EPS.Type.JD)
-eigensolver.setProblemType(SLEPc.EPS.ProblemType.GHEP)
-eigensolver.setWhichEigenpairs(eigensolver.Which.SMALLEST_REAL)
-eigensolver.setDimensions(n)
-eigensolver.solve()
-vr, vi = A.getVecs()
+dim = domain.topology.dim - 1
+domain.topology.create_connectivity(dim, domain.topology.dim)
 
 
-for i in range(1,eigensolver.getConverged()):
-    lmbda = eigensolver.getEigenpair(i, vr, vi)
-    print(i)
-    print(lmbda/(np.pi))
+boundary = np.where(np.array(dolfinx.cpp.mesh.exterior_facet_indices(domain.topology)) == 1)[0]
+boundary_dofs = dolfinx.fem.locate_dofs_topological(V, dim, boundary)
+
+N = V.dofmap.index_map.size_global * V.dofmap.index_map_bs
+
+
+freeinds = np.setdiff1d(range(N),boundary_dofs,assume_unique=True).astype(np.int32)
+
+
+#%%
+from dolfinx.fem.petsc import assemble_matrix
+# A = dolfinx.fem.assemble_matrix(a)
+
+A = assemble_matrix(a)
+A.assemble()
+
+ai, aj, av = A.getValuesCSR()
+A = sps.csr_matrix((av, aj, ai))[freeinds,:][:,freeinds]
+
+na = A.toarray()
+
+B = assemble_matrix(b)
+B.assemble()
+
+bi, bj, bv = B.getValuesCSR()
+B = sps.csr_matrix((bv, bj, bi))[freeinds,:][:,freeinds]
+
+nb = B.toarray()
+
+# na = petsc2array(A)
+# nb = petsc2array(B)
+#%%
+
+import scipy.sparse.linalg as la
+
+# Solver
+k=5
+vals, vecs = la.eigs(na, k=k, M=nb, sigma=0)
+ths=np.pi**2*np.array([2,5,5,8,10])
+
+print("Theoretical value | Calculated one | Difference")
+for i in range(k): print(f"{ths[i]:1.14f}"+" | "+f"{np.real(vals[i]):.4f}"+f"{np.imag(vals[i]):+.0e}"+"j | "+f"{np.abs(ths[i]-vals[i]):00.2e}")
 # %%
